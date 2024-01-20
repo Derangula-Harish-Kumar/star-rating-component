5941bf4b188b0a3b1370a0a59f96fab2b2a43dc3
Derangula-Harish-Kumar <dharishk8@gmail.com>
2024-01-20 17:54:50 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,129 @@
+// write your JavaScript here
+let star1 = document.querySelector("#star1")
+let star2 = document.querySelector("#star2")
+let star3 = document.querySelector("#star3")
+let star4 = document.querySelector("#star4")
+let star5 = document.querySelector("#star5")
+let text = document.querySelector(".botom p")
+
+// let allstars = document.querySelectorAll(".fa-solid fa-star")
+
+
+
+// allstars.forEach(function(){
+//     star1.style.color = "#fccd16";
+//         star2.style.color = "#e5eafb";
+//         star3.style.color = "#e5eafb";
+//         star4.style.color = "#e5eafb";
+//         star5.style.color = "#e5eafb";
+// })
+// forEach()
+
+// star1 = 1;
+// if(star1 = 1){
+//     star1.style.color = "#fccd16";
+//         star2.style.color = "#e5eafb";
+//         star3.style.color = "#e5eafb";
+//         star4.style.color = "#e5eafb";
+//         star5.style.color = "#e5eafb";
+//         text.innerHTML = `we are sorry hear that you had a bad exprience. we would like to
+//          learn about what hapend how we can make this right`
+// }else{
+//     display = #e5eafb;
+// }
+
+// function star_1() {
+//     star1.addEventListener("click",function(){
+//         star1.style.color = "#fccd16";
+//         star2.style.color = "#e5eafb";
+//         star3.style.color = "#e5eafb";
+//         star4.style.color = "#e5eafb";
+//         star5.style.color = "#e5eafb";
+//         text.innerHTML = `we are sorry hear that you had a bad exprience. we would like to
+//          learn about what hapend how we can make this right`
+     
+//     })
+    
+// }
+
+star1.addEventListener("click",function(){
+    star1.style.color = "#fccd16";
+    star2.style.color = "#e5eafb";
+    star3.style.color = "#e5eafb";
+    star4.style.color = "#e5eafb";
+    star5.style.color = "#e5eafb";
+    text.innerHTML = `we are sorry hear that you had a bad exprience. we would like to
+     learn about what hapend how we can make this right`
+    
+})
+
+
+star2.addEventListener("click",function(){
+    star1.style.color = "#fccd16";
+    star2.style.color = "#fccd16";
+    star3.style.color = "#e5eafb";
+    star4.style.color = "#e5eafb";
+    star5.style.color = "#e5eafb";
+    text.innerHTML = `We apologize for the inconvenience you experienced. We appreciate 
+    you feedback and would like to work you to address any issues.`
+    
+})
+
+star3.addEventListener("click",function(){
+    star1.style.color = "#fccd16";
+    star2.style.color = "#fccd16";
+    star3.style.color = "#fccd16";
+    star4.style.color = "#e5eafb";
+    star5.style.color = "#e5eafb";
+    text.innerHTML = `Thank you for uout feedback. We're sorry to heare that you experience
+    wasn't perfect. We would love to hear more about your concerns to see
+    how we can improve.`
+    
+})
+
+star4.addEventListener("click",function(){
+    star1.style.color = "#fccd16";
+    star2.style.color = "#fccd16";
+    star3.style.color = "#fccd16";
+    star4.style.color = "#fccd16";
+    star5.style.color = "#e5eafb";
+    text.innerHTML = `Thank you for uout feedback. We're glad to know that you had a great 
+    experience and we appreciate you support. `
+    
+})
+
+star5.addEventListener("click",function(){
+    star1.style.color = "#fccd16";
+    star2.style.color = "#fccd16";
+    star3.style.color = "#fccd16";
+    star4.style.color = "#fccd16";
+    star5.style.color = "#fccd16";
+    text.innerHTML = `Excellent! We're thrilled to hear you had such a positive experience.
+    Thank you for choosing our platform`
+    
+})
+
+
+// let names = [
+//     'hari','ramesh','suresh','kirn','prashath'
+// ]
+
+// // for(let i = 0; i< names.length ; i++){
+// //     console.log(names[i]);
+// // }
+
+// // function nme(ox){
+// //     console.log(nme);
+// // };
+
+
+// // names.forEach(function(item){
+// //     console.log(item);
+// // });
+
+
+
+
+// names.forEach((dady) => {
+//     console.log(dady);
+// });
